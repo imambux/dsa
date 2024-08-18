8c550f4df8f95f283403f656bf01dc47ff232704
imambux <imam.bux@scout24.com>
2024-08-18 22:26:35 +0200

    Taylor Series

--- src/main/java/TaylorSeries.java
+++ src/main/java/TaylorSeries.java
@@ -11,18 +11,47 @@ public class TaylorSeries {
     static double power = 1;
     static double factorial = 1;
 
-    public static double taylorSeries(int x, int n) {
+    static double sum = 1;
+
+    public static double rTaylorSeries(int x, int n) {
         if (n == 0) return 1;
 
-        double result = taylorSeries(x, n - 1);
+        double result = rTaylorSeries(x, n - 1);
         power *= x;
         factorial *= n;
 
         return result + (power/factorial);
     }
 
+    /*
+    * This is the optimized version of Taylor Series
+    * */
+    public static double rTaylorSeries2(int x, int n) {
+        if (n == 0) return sum;
+
+        sum = 1 + (double) x / n * sum;
+
+        return rTaylorSeries2(x, n - 1);
+    }
+
+    /*
+    * This is the optimized version of Taylor Series
+    * T = O(n)
+    * */
+    public static double iTaylorSeries2(int x, int n) {
+        double result = 1;
+
+        for (; n > 0; n--) {
+            result = 1 + (double) x / n * result;
+        }
+
+        return result;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Taylor Series: " + taylorSeries(3, 10));
+        System.out.println("Taylor Series: " + rTaylorSeries(3, 10));
+        System.out.println("Taylor Series: " + rTaylorSeries2(3, 10));
+        System.out.println("Taylor Series: " + iTaylorSeries2(3, 10));
     }
 
 }
