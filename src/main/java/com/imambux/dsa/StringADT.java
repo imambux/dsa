e24262a34061a36202f8356ce839e317e044de63
imambux <imam.bux@scout24.com>
2024-09-07 23:55:00 +0200

    StringADT - change case, count vowels, consonants, count words

--- src/main/java/com/imambux/dsa/StringADT.java
+++ src/main/java/com/imambux/dsa/StringADT.java
@@ -14,9 +14,50 @@ public class StringADT {
         System.out.println(characters);
     }
 
+    public static void countVowelsAndConsonants(char[] characters) {
+        int vowelsCount = 0;
+        int consonantsCount = 0;
+
+        for (char character : characters) {
+            if (character == 'A'
+                || character == 'a'
+                || character == 'E'
+                || character == 'e'
+                || character == 'I'
+                || character == 'i'
+                || character == 'O'
+                || character == 'o'
+                || character == 'U'
+                || character == 'u'
+            ) {
+                vowelsCount++;
+            } else if((character >= 65 && character <= 90)
+                || (character >= 97 && character <= 122)
+            ) {
+                consonantsCount++;
+            }
+        }
+
+        System.out.printf("Vowels: %d, Consonants: %d", vowelsCount, consonantsCount);
+    }
+
+    public static void countWords(String string) {
+        int wordCount = 0;
+        for (int i = 0; i < string.length(); i++) {
+            if (string.charAt(i) == ' ' && string.charAt(i - 1) != ' ') {
+                wordCount++;
+            }
+        }
+
+        System.out.printf("Totals Words: %d", wordCount + 1);
+    }
+
+
     public static void main(String[] args) {
-        // Change Case
-        changeCase("WelcomE!!!aBc".toCharArray());
+//        changeCase("WelcomE!!!aBc".toCharArray());
+//        countVowelsAndConsonants(new char[]{'I', 'm', 'a', 'm', '!'});
+//        countWords("Hello there, my name    is Imam Bux.");
+
     }
 
 }
