465d5eda895a09500e8345d305df94359ae80152
imambux <imam.bux@scout24.com>
2024-09-08 09:44:42 +0200

    StringADT - anagram, find duplicates for lower case using bitwise operator

--- src/main/java/com/imambux/dsa/StringADT.java
+++ src/main/java/com/imambux/dsa/StringADT.java
@@ -69,13 +69,78 @@ public class StringADT {
         System.out.printf("Totals Words: %d", wordCount + 1);
     }
 
+    public static void findDuplicatesForLowerCaseOnly(String string) {
+        int[] lowerCaseCharacters = new int[26];
+
+        for (int i = 0; i < string.length() - 1; i++) {
+            lowerCaseCharacters[string.charAt(i) - 97]++;
+        }
+
+        for (int i = 0; i < lowerCaseCharacters.length; i++) {
+            if (lowerCaseCharacters[i] > 1) {
+                System.out.printf("%c:%d%n", i + 97, lowerCaseCharacters[i]);
+            }
+        }
+    }
+
+    public static void findDuplicatesForLowerCaseOnlyUsingBitwiseOperator(String string) {
+        // Take 26 bits to accomodate all lower case english characters
+        // We can take 32 bits instead = Integer = 32 bits = 4 bytes but we are only interested in the first 26 bits
+        int H = 0; // to make all bits 0
+
+        // loop through each character
+        for(int i = 0; i < string.length(); i++) {
+            // take a variable that has 1 bit ON
+            int current = 1;
+
+            // Take the ASCII of the character from s and subtract with 97 to make it adjustable with 0-25 bit positions
+            // Get the bit position and left shift it
+            current = current << string.charAt(i) - 97;
+
+            // check if the bit is already ON using Bitwise AND operator (aka Masking = find the bit is ON or not)
+            // Which means it is already present and is duplicate
+            if ((H & current) != 0) {
+                System.out.printf("%c ", string.charAt(i));
+                continue;
+            }
+
+            // if the bit was not ON already, make it ON by using Bitwise OR operator (aka Merging = to set the value)
+            H = H | current;
+        }
+    }
+
+    /*
+    * Anagram, compare two strings having same alphabets but different words e.g. medical, decimal are anagram
+    * This method works with distinct characters only
+    * */
+    public static boolean isAnagram(String str1, String str2) {
+        // If it's a different size, two words cannot anagram
+        if (str1.length() != str2.length()) return false;
+
+        boolean[] hashTable = new boolean[128];
+
+        // Mark the ASCII positions for str1
+        for (int i = 0; i < str1.length(); i++) {
+            hashTable[str1.charAt(i) - 97] = true;
+        }
+
+        // Check str2 that all the characters are marked as true in the hash table
+        // If any character is not marked as true, then two words are not anagram
+        for (int i = 0; i < str2.length(); i++) {
+            if (!hashTable[str2.charAt(i) - 97]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
 
     public static void main(String[] args) {
 //        changeCase("WelcomE!!!aBc".toCharArray());
 //        countVowelsAndConsonants(new char[]{'I', 'm', 'a', 'm', '!'});
 //        countWords("Hello there, my name    is Imam Bux.");
 //        reverseString("Imam");
-
+        findDuplicatesForLowerCaseOnly("finding");
     }
 
 }
